8a2f961bb6f0dc139cccd9f9b957148df730876d
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 05:08:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getRepositories(){
   req.open("GET","https://api.github.com/users/"+username+"/repos");
   req.send();
 }
+
+function showRespositories(){
+  let dest=document.getElementById("repositories");
+  const repos=JSON.parse(this.responseText);
+  const output=
+  
+}
