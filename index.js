7586c17e377a7a36fa3fa401dca6d64b7e142403
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 05:24:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function showRepositories(){
   let dest=document.getElementById("repositories");
   const repos=JSON.parse(this.responseText);
   console.log(repos.length);
-  const output=`<ul>${repos.map(r=>"<li>"+r.name+" - <a href='#' data-repo='"+r.name+"' onclick='getCommits(this)>Get Commits</a></li>").join("")}`;
+  const output=`<ul>${repos.map(r=>"<li>"+r.name+" - <a href='#' data-repo='"+r.name+"' onclick='getCommits(this)>Get Commits</a></li>").join("")}</ul>`;
   dest.innerHTML=output;
 }
