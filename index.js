41497e40d4e1b3792204c7a367db04b02d2bbfa3
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 05:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getRepositories(){
-  let username=document.getElementsByName("username")[0].value;
+  const username=document.getElementsByName("username")[0].value;
   const req=new XMLHttpRequest();
   req.addEventListener("load",showRepositories);
   req.open("GET","https://api.github.com/users/"+username+"/repos");
