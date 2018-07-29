58d4f985858722125f0a7e0bbc96c6902fb7062c
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 05:20:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getRepositories(){
   req.send();
 }
 
-function showRespositories(){
+function showRepositories(){
   let dest=document.getElementById("repositories");
   const repos=JSON.parse(this.responseText);
   const output=`<ul>${dest.map(r=>"<li>"+r.name+" - <a href='#' data-repo='"+r.name+"' onclick='getCommits(this)>Get Commits</a><li>").join("")}`;
